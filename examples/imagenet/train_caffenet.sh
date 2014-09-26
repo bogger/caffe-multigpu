e9dce2d395a842ca4937a5d96cdfa71197c8de40
zhirongw <xavibrowu@gmail.com>
2014-09-26 07:38:38 -0400

    just modified model parameters, no core code touched

--- examples/imagenet/train_caffenet.sh
+++ examples/imagenet/train_caffenet.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+GOOGLE_LOG_DIR="models/bvlc_reference_caffenet" \
 ./build/tools/caffe train \
-    --solver=models/bvlc_reference_caffenet/solver.prototxt
-    --log_dir=.
+    --solver=models/bvlc_reference_caffenet/solver.prototxt \
+    --gpu=1
--- examples/imagenet/train_googlenet.sh
+++ examples/imagenet/train_googlenet.sh
@@ -2,4 +2,5 @@
 
 GOOGLE_LOG_DIR=models/googlenet \
 ./build/tools/caffe train \
-    --solver=models/googlenet/solver.prototxt
+    --solver=models/googlenet/solver.prototxt \
+    --gpu=2
