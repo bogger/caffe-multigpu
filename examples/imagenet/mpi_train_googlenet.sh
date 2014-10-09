310aa2cf5fb996722ca937e2efaafbd22a05eca6
zhirongw <xavibrowu@gmail.com>
2014-10-09 08:06:42 -0400

    mpi config changes

--- examples/imagenet/mpi_train_googlenet.sh
+++ examples/imagenet/mpi_train_googlenet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 #GOOGLE_LOG_DIR=models/googlenet \
-mpirun -np 2 ./build/tools/caffe train \
+mpirun -np 4 ./build/tools/caffe train \
     --solver=models/googlenet/solver.prototxt \
 #    --gpu=3
--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -289,6 +289,7 @@ int main(int argc, char** argv) {
 #ifdef USE_MPI
   int my_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  LOG(INFO) << "rank: " << my_rank;
 #endif
 
 
@@ -298,9 +299,10 @@ int main(int argc, char** argv) {
 #ifdef USE_MPI
 //  FLAGS_gpu = my_rank;
   switch (my_rank){
-  case 0: FLAGS_gpu=2;break;
-  case 1: FLAGS_gpu=3;break;
+  case 0: FLAGS_gpu=3;break;
+  case 1: FLAGS_gpu=2;break;
   case 2: FLAGS_gpu=1;break;
+  case 3: FLAGS_gpu=0;break;
   }
   LOG(INFO)<<FLAGS_gpu;
 #endif
