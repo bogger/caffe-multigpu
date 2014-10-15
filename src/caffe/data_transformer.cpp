6009030316d0f4e638613cdbdf6f72b70ee660c1
puzzledqs <arthur.qiushi@gmail.com>
2014-10-15 16:20:52 +0800

    streamline opencv header includes

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -1,16 +1,12 @@
 #include <string>
 #include <opencv2/core/core_c.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/highgui/highgui_c.h>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/imgproc_c.h>
 
 #include "caffe/data_transformer.hpp"
 #include "caffe/util/math_functions.hpp"
 #include "caffe/util/rng.hpp"
 
-using namespace cv;
 namespace caffe {
 
 template<typename Dtype>
--- src/caffe/layers/compact_data_layer.cpp
+++ src/caffe/layers/compact_data_layer.cpp
@@ -5,10 +5,7 @@
 #include <vector>
 
 #include <opencv2/core/core_c.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/highgui/highgui_c.h>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/imgproc_c.h>
 
 #include "caffe/common.hpp"
@@ -25,7 +22,6 @@
 using namespace boost::filesystem;
 #endif
 
-using namespace cv;
 
 namespace caffe {
 
