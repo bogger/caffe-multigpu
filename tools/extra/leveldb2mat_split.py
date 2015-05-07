637f55c126dff222a18e93382e6d3726b9b27dae
bogger <yljatthu@gmail.com>
2015-05-07 19:38:21 +0800

    modify the data type in "leveldb2mat" and "leveldb2mat_split" to float.

--- tools/extra/leveldb2mat_split.py
+++ tools/extra/leveldb2mat_split.py
@@ -3,32 +3,39 @@ import feat_helper_pb2
 import numpy as np
 import scipy.io as sio
 import time
-
 def main(argv):
 	leveldb_name = sys.argv[1]
 	print "%s" % sys.argv[1]
 	print "%s" % sys.argv[2]
 	print "%s" % sys.argv[3]
 	print "%s" % sys.argv[4]
+	print "%s" % sys.argv[5]
         # window_num = 1000;
         # window_num = 12736;
-        window_num = int(sys.argv[2]);
+	window_num = int(sys.argv[2])
+	sample_len = int(sys.argv[3])
         # window_num = 2845;
 
-	start = time.time()
+	
 	if 'db' not in locals().keys():
 		db = leveldb.LevelDB(leveldb_name)
 		datum = feat_helper_pb2.Datum()
 
-	ft = np.zeros((window_num, int(sys.argv[3])),dtype=np.float32)
+	
+	split = int(sys.argv[4])
+	per_batch = int(np.ceil(float(window_num) / split))
+	for i in range(split):
+		start = i*per_batch
+		final = min((i+1)*per_batch,window_num)
+		ft = np.zeros((final-start, sample_len),,dtype=np.float32)
+		for im_idx in range(start,final):
+			datum.ParseFromString(db.Get("%08d" %(im_idx)))
+			ft[im_idx-start, :] = datum.float_data
 
-	for im_idx in range(window_num):
-		datum.ParseFromString(db.Get("%08d" %(im_idx)))
-		ft[im_idx, :] = datum.float_data
+		
+		sio.savemat(sys.argv[5]+'_'+str(i+1), {'feats':ft},oned_as='row')
 
-	print 'time 1: %f' %(time.time() - start)
-	sio.savemat(sys.argv[4], {'feats':ft},oned_as='row')
-	print 'time 2: %f' %(time.time() - start)
+	
 	print 'done!'
 
 	#leveldb.DestroyDB(leveldb_name)
